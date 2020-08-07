693c76cc31e4e27faefd86ef9baa0e261a99f908
rahmat hidayat <mhat89@users.noreply.github.com>
2020-08-07 15:06:08 +0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1DbaUNmYGOzvfoPcre9VgQ_J7-5aRVu29/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/12GRaDtQJwPyjIQjPgB1L-z9jgGBkkywESuWv8njgbEQ/';
